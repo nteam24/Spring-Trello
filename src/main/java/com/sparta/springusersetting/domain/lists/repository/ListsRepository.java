1a4d22187cbad668229e6264528549584a6e619e
MinJun Song <j34679@naver.com>
2024-10-14 19:04:36 +0900

    corrcet

--- src/main/java/com/sparta/springusersetting/domain/lists/repository/ListsRepository.java
+++ src/main/java/com/sparta/springusersetting/domain/lists/repository/ListsRepository.java
@@ -3,6 +3,6 @@ package com.sparta.springusersetting.domain.lists.repository;
 import com.sparta.springusersetting.domain.lists.entity.Lists;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ListRepository extends JpaRepository<Lists, Long> {
+public interface ListsRepository extends JpaRepository<Lists, Long> {
 
 }

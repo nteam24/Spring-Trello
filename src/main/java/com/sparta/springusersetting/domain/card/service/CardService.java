09d9805b93686fd747719a49ca8d7c2a91933a85
MinJun Song <j34679@naver.com>
2024-10-14 21:19:11 +0900

    crud

--- src/main/java/com/sparta/springusersetting/domain/card/service/CardService.java
+++ src/main/java/com/sparta/springusersetting/domain/card/service/CardService.java
@@ -12,6 +12,7 @@ import com.sparta.springusersetting.domain.user.entity.User;
 import com.sparta.springusersetting.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -32,13 +33,38 @@ public class CardService {
     }
 
     public CardResponseDto getCard(AuthUser authUser, Long cardId) {
-
-
+        Card card = cardRepository.findById(cardId).orElse(null);
+        CardResponseDto cardResponseDto = new CardResponseDto(
+                card.getId(),
+                card.getTitle(),
+                card.getContents(),
+                card.getDeadline(),
+                card.getManager().getEmail(),
+                card.getActivityLogs().toString(),
+                card.getComments());
+        return cardResponseDto;
     }
 
-    public void updateCard() {
+
+    @Transactional
+    public void updateCard(AuthUser authUser, CardRequestDto requestDto, Long cardId)
+    {
+        User manager = userRepository.findById(requestDto.getManagerId()).orElse(null);
+        Lists lists = listsRepository.findById(requestDto.getListId()).orElse(null);
+        Card card = cardRepository.findById(cardId).orElse(null);
+        card.update(manager,lists,requestDto.getTitle(),requestDto.getContents(),requestDto.getDeadline());
+        cardRepository.save(card);
     }
 
-    public void deleteCard() {
+    public void deleteCard(AuthUser authUser, Long cardId) {
+        Card card = cardRepository.findById(cardId).orElse(null);
+        cardRepository.delete(card);
+    }
+
+    public Card findCard(AuthUser authUser, Long cardId)
+    {
+        Card card = cardRepository.findById(cardId).orElseThrow(null);
+        return card;
+
     }
 }
--- src/main/java/com/sparta/springusersetting/domain/comment/entity/Comment.java
+++ src/main/java/com/sparta/springusersetting/domain/comment/entity/Comment.java
@@ -1,4 +1,16 @@
 package com.sparta.springusersetting.domain.comment.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }

305928607f614a229c6f20fb4b04477b356ea576
MinJun Song <j34679@naver.com>
2024-10-15 02:02:01 +0900

    NotFoundCardException

--- src/main/java/com/sparta/springusersetting/domain/card/service/CardService.java
+++ src/main/java/com/sparta/springusersetting/domain/card/service/CardService.java
@@ -4,10 +4,13 @@ package com.sparta.springusersetting.domain.card.service;
 import com.sparta.springusersetting.domain.card.dto.CardRequestDto;
 import com.sparta.springusersetting.domain.card.dto.CardResponseDto;
 import com.sparta.springusersetting.domain.card.entity.Card;
+import com.sparta.springusersetting.domain.card.exception.NotFoundCardException;
+import com.sparta.springusersetting.domain.card.repository.ActivityLogRepository;
 import com.sparta.springusersetting.domain.card.repository.CardRepository;
 import com.sparta.springusersetting.domain.common.dto.AuthUser;
 import com.sparta.springusersetting.domain.lists.entity.Lists;
 import com.sparta.springusersetting.domain.lists.repository.ListsRepository;
+import com.sparta.springusersetting.domain.lists.service.ListsService;
 import com.sparta.springusersetting.domain.user.entity.User;
 import com.sparta.springusersetting.domain.user.enums.MemberRole;
 import com.sparta.springusersetting.domain.user.repository.UserRepository;
@@ -23,6 +26,8 @@ public class CardService {
     private final CardRepository cardRepository;
     private final UserService userService;
     private final ListsRepository listsRepository;
+    private final ActivityLogRepository activityLogRepository;
+
 
     public void createCard(AuthUser authUser, CardRequestDto requestDto)
     {
@@ -37,7 +42,7 @@ public class CardService {
     }
 
     public CardResponseDto getCard(AuthUser authUser, Long cardId) {
-        Card card = cardRepository.findById(cardId).orElse(null);
+        Card card = cardRepository.findById(cardId).orElseThrow(NotFoundCardException::new);
         CardResponseDto cardResponseDto = new CardResponseDto(
                 card.getId(),
                 card.getTitle(),
@@ -55,19 +60,20 @@ public class CardService {
     {
         User manager = userService.findUser(requestDto.getManagerId());
         Lists lists = listsRepository.findById(requestDto.getListId()).orElse(null);
-        Card card = cardRepository.findById(cardId).orElse(null);
+        Card card = cardRepository.findById(cardId).orElseThrow(NotFoundCardException::new);
         card.update(manager,lists,requestDto.getTitle(),requestDto.getContents(),requestDto.getDeadline());
+
         cardRepository.save(card);
     }
 
     public void deleteCard(AuthUser authUser, Long cardId) {
-        Card card = cardRepository.findById(cardId).orElse(null);
+        Card card = cardRepository.findById(cardId).orElseThrow(NotFoundCardException::new);
         cardRepository.delete(card);
     }
 
     public Card findCard(AuthUser authUser, Long cardId)
     {
-        Card card = cardRepository.findById(cardId).orElseThrow(null);
+        Card card = cardRepository.findById(cardId).orElseThrow(NotFoundCardException::new);
         return card;
 
     }

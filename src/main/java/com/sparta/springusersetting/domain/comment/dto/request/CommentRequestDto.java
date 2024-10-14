aceb76dd0025bedbfc80b4b526484a48305f3373
Daegyu Han <bigstar77han@gmail.com>
2024-10-14 20:40:05 +0900

    [update] AuthUser Id Type 수정

--- src/main/java/com/sparta/springusersetting/domain/comment/dto/request/CommentRequestDto.java
+++ src/main/java/com/sparta/springusersetting/domain/comment/dto/request/CommentRequestDto.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class CommentCreateRequestDto {
+public class CommentRequestDto {
     private String content;
     private CommentEmoji commentEmoji;
 }

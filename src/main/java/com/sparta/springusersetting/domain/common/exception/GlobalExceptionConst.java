41a227acb85e9bd39aa9a77e8a2b56f718daaf58
Daegyu Han <bigstar77han@gmail.com>
2024-10-14 23:20:40 +0900

    [update] Create Comment, Update Comment 기능 추가

--- src/main/java/com/sparta/springusersetting/domain/common/exception/GlobalExceptionConst.java
+++ src/main/java/com/sparta/springusersetting/domain/common/exception/GlobalExceptionConst.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum GlobalExceptionConst {
 
     // 상태코드 400
-    DUPLICATE_PASSWORD(HttpStatus.BAD_REQUEST, "새 비밀번호는 이전에 사용한 비밀번호와 같을 수 없습니다."),
-    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다."),
+    DUPLICATE_PASSWORD(HttpStatus.BAD_REQUEST, " 새 비밀번호는 이전에 사용한 비밀번호와 같을 수 없습니다."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, " 새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다."),
 
     // 상태코드 401
     UNAUTHORIZED_PASSWORD(HttpStatus.UNAUTHORIZED, " 비밀번호를 확인해주세요."),
@@ -18,6 +18,8 @@ public enum GlobalExceptionConst {
     // 상태코드 404
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, " 회원이 존재하지 않습니다."),
     NOT_FOUND_EMAIL(HttpStatus.NOT_FOUND, " 이메일을 확인해주세요."),
+    NOT_FOUND_COMMENT(HttpStatus.NOT_FOUND, " 존재하지 않는 댓글입니다."),
+    
 
     // 상태코드 409
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, " 중복된 이메일입니다.");

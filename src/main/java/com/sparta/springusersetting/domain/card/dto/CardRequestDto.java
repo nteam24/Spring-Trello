5f19283041224af2c7839ceee9884cde9ab744be
MinJun Song <j34679@naver.com>
2024-10-14 18:56:57 +0900

    correct Lists ManyToOne

---
+++ src/main/java/com/sparta/springusersetting/domain/card/dto/CardRequestDto.java
@@ -0,0 +1,16 @@
+package com.sparta.springusersetting.domain.card.dto;
+
+
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class CardRequestDto {
+
+    private Long managerId;
+    private Long listId;
+    private String title;
+    private String contents;
+    private LocalDate deadline;
+}

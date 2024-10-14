38976dc86fa0412e3260f67d6551295628638740
Changmin Kim <ksjchm4@gmail.com>
2024-10-14 20:12:26 +0900

    [FEAT] Workspace 관리자 등록시 예외처리 추가

--- src/main/java/com/sparta/springusersetting/domain/common/exception/GlobalExceptionConst.java
+++ src/main/java/com/sparta/springusersetting/domain/common/exception/GlobalExceptionConst.java
@@ -9,8 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum GlobalExceptionConst {
 
     // 상태코드 400
-    DUPLICATE_PASSWORD(HttpStatus.BAD_REQUEST, "새 비밀번호는 이전에 사용한 비밀번호와 같을 수 없습니다."),
-    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다."),
+    DUPLICATE_PASSWORD(HttpStatus.BAD_REQUEST, " 새 비밀번호는 이전에 사용한 비밀번호와 같을 수 없습니다."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, " 새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다."),
+    INVALID_ROLE(HttpStatus.BAD_REQUEST, " 올바른 권한이 아닙니다."),
+    ALREADY_ADMIN(HttpStatus.BAD_REQUEST, " 이미 해당 워크스페이스의 관리자 입니다."),
 
     // 상태코드 401
     UNAUTHORIZED_PASSWORD(HttpStatus.UNAUTHORIZED, " 비밀번호를 확인해주세요."),
@@ -19,6 +21,7 @@ public enum GlobalExceptionConst {
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, " 회원이 존재하지 않습니다."),
     NOT_FOUND_EMAIL(HttpStatus.NOT_FOUND, " 이메일을 확인해주세요."),
     NOT_FOUND_WORKSPACE(HttpStatus.NOT_FOUND, " 워크스페이스가 존재하지 않습니다."),
+    NOT_FOUND_USER_WORKSPACE(HttpStatus.NOT_FOUND, " 해당 유저는 해당 워크스페이스 소속이 아닙니다."),
 
     // 상태코드 409
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, " 중복된 이메일입니다."),

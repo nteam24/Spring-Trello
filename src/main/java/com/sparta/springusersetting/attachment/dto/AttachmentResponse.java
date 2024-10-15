c3a8c4cc5a4dcc428abb55455d6c360f9fc2e092
MinJun Song <j34679@naver.com>
2024-10-15 22:01:56 +0900

    s3

--- src/main/java/com/sparta/springusersetting/attachment/dto/AttachmentResponse.java
+++ src/main/java/com/sparta/springusersetting/attachment/dto/AttachmentResponse.java
@@ -1,7 +1,9 @@
 package com.sparta.springusersetting.attachment.dto;
 
 import com.sparta.springusersetting.attachment.entity.Attachment;
+import lombok.Getter;
 
+@Getter
 public class AttachmentResponse {
     private Long id;
     private String s3Url;

18a66ceb470a8bf5e1f5e634cb1bea21989b9711
MinJun Song <77713554+Luta13@users.noreply.github.com>
2024-10-16 20:57:31 +0900

    Update CardService.java

--- src/main/java/com/sparta/springusersetting/domain/card/service/CardService.java
+++ src/main/java/com/sparta/springusersetting/domain/card/service/CardService.java
@@ -1,63 +1,134 @@
-package com.sparta.springusersetting.domain.card.controller;
+package com.sparta.springusersetting.domain.card.service;
 
 
 import com.sparta.springusersetting.attachment.service.AttachmentService;
-import com.sparta.springusersetting.config.ApiResponse;
-import com.sparta.springusersetting.domain.card.dto.CardRequestDto;
-import com.sparta.springusersetting.domain.card.dto.CardResponseDto;
-import com.sparta.springusersetting.domain.card.dto.CardSearchRequestDto;
-import com.sparta.springusersetting.domain.card.dto.CardSearchResponseDto;
-import com.sparta.springusersetting.domain.card.service.CardService;
+import com.sparta.springusersetting.domain.card.dto.*;
+import com.sparta.springusersetting.domain.card.entity.Card;
+import com.sparta.springusersetting.domain.card.exception.BadAccessCardException;
+import com.sparta.springusersetting.domain.card.repository.CardRepository;
 import com.sparta.springusersetting.domain.common.dto.AuthUser;
-import jakarta.validation.Valid;
+import com.sparta.springusersetting.domain.lists.entity.Lists;
+import com.sparta.springusersetting.domain.lists.repository.ListsRepository;
+import com.sparta.springusersetting.domain.participation.service.MemberManageService;
+import com.sparta.springusersetting.domain.user.entity.User;
+import com.sparta.springusersetting.domain.user.enums.MemberRole;
+import com.sparta.springusersetting.domain.user.exception.BadAccessUserException;
+import com.sparta.springusersetting.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 
-@RestController
+
+@Service
 @RequiredArgsConstructor
-@RequestMapping("/cards")
-public class CardController {
+public class CardService {
 
-    private final CardService cardService;
 
-    @PostMapping
-    public ResponseEntity<ApiResponse<String>> createCard(@AuthenticationPrincipal AuthUser authUser,
-                                                          @Valid @RequestPart CardRequestDto card,
-                                                          @RequestPart(name = "file", required = false) MultipartFile file)
+    private final CardRepository cardRepository;
+    private final UserService userService;
+    private final ListsRepository listsRepository;
+    private final MemberManageService memberManageService;
+    private final AttachmentService attachmentService;
 
+
+    @Transactional
+    public String createCard(AuthUser authUser, CardRequestDto requestDto, MultipartFile file)
     {
+        Lists lists = listsRepository.findById(requestDto.getListId()).orElse(null);
+        User createUser = User.fromAuthUser(authUser);
+        if(memberManageService.checkMemberRole(createUser.getId(),lists.getBoard().getWorkspace().getId()) == MemberRole.ROLE_READ_USER)
+        {
+            throw new BadAccessUserException();
+        }
 
-        return ResponseEntity.ok(ApiResponse.success(cardService.createCard(authUser,card,file)));
+        User manager = userService.findUser(requestDto.getManagerId());
+        Card card = new Card(manager, lists, requestDto.getTitle(), requestDto.getContents(), requestDto.getDeadline());
+        cardRepository.save(card);
+        if(file != null && !file.isEmpty()) {
+            try {
+                attachmentService.saveFile(authUser, card.getId(), file);
+            } catch (IOException e)
+            {
+                throw new RuntimeException("파일 저장중 오류 발생");
+            }
+        }
+        return "카드 생성이 완료되었습니다.";
     }
 
-    @GetMapping("/{cardId}")
-    public ResponseEntity<ApiResponse<CardResponseDto>> getCard(@AuthenticationPrincipal AuthUser authUser, @PathVariable Long cardId)
-    {
-        return ResponseEntity.ok(ApiResponse.success(cardService.getCard(authUser,cardId)));
+    @Transactional(readOnly = true)
+    public CardResponseDto getCard(AuthUser authUser, Long cardId) {
+        if(authUser == null)
+            throw new BadAccessUserException();
+
+        Card card = cardRepository.findById(cardId).orElse(null);
+        CardResponseDto cardResponseDto = new CardResponseDto(
+                card.getId(),
+                card.getTitle(),
+                card.getContents(),
+                card.getDeadline(),
+                card.getManager().getEmail(),
+                card.getActivityLogs().stream().map(ActivityLogResponseDto::new).toList(),
+                card.getCommentList());
+        return cardResponseDto;
     }
 
-    @PatchMapping("/{cardId}")
-    public ResponseEntity<ApiResponse<String>> updateCard(@AuthenticationPrincipal AuthUser authUser, @Valid @RequestBody CardRequestDto card, @PathVariable Long cardId)
+
+    @Transactional
+    public String updateCard(AuthUser authUser, CardRequestDto requestDto, Long cardId)
     {
-        return ResponseEntity.ok(ApiResponse.success(cardService.updateCard(authUser,card,cardId)));
+        Lists lists = listsRepository.findById(requestDto.getListId()).orElse(null);
+        User createUser = User.fromAuthUser(authUser);
+
+        if(memberManageService.checkMemberRole(createUser.getId(),lists.getBoard().getWorkspace().getId()) == MemberRole.ROLE_READ_USER)
+        {
+            throw new BadAccessUserException();
+        }
+
+        User manager = userService.findUser(requestDto.getManagerId());
+        Card card = cardRepository.findById(cardId).orElse(null);
+        card.update(manager,lists,requestDto.getTitle(),requestDto.getContents(),requestDto.getDeadline());
+        cardRepository.save(card);
+
+        return "카드 수정이 완료되었습니다.";
+    }
+    @Transactional
+    public String deleteCard(AuthUser authUser, Long cardId) {
+        User deletedUser = User.fromAuthUser(authUser);
+        Card card = cardRepository.findById(cardId).orElse(null);
+        if(memberManageService.checkMemberRole(deletedUser.getId(),card.getLists().getBoard().getWorkspace().getId()) == MemberRole.ROLE_READ_USER)
+        {
+            throw new BadAccessUserException();
+        }
+
+        cardRepository.delete(card);
+        return "카드 삭제가 완료되었습니다.";
+    }
+    @Transactional(readOnly = true)
+    public Slice<CardSearchResponseDto> searchCard(Long userId, CardSearchRequestDto searchRequest, Long cursorId, int pageSize) {
+        User user = userService.findUser(userId);
+
+        if (searchRequest.getWorkspaceId() == null) {
+            throw new BadAccessCardException();
+        }
+
+        // 사용자가 해당 워크스페이스에 속해 있는지 확인
+        MemberRole memberRole = memberManageService.checkMemberRole(userId, searchRequest.getWorkspaceId());
+        if (memberRole == null) {
+            throw new BadAccessCardException();
+        }
+
+        // QueryDSL을 사용한 검색 수행
+        return cardRepository.searchCards(searchRequest, cursorId, pageSize);
     }
 
-    @DeleteMapping("/{cardId}")
-    public ResponseEntity<ApiResponse<String>> deleteCard(@AuthenticationPrincipal AuthUser authUser,@PathVariable Long cardId)
-    {
-        return ResponseEntity.ok(ApiResponse.success(cardService.deleteCard(authUser,cardId)));
-    }
+    public Card findCard(Long cardId) {
+        Card card = cardRepository.findById(cardId).orElseThrow(null);
+        return card;
 
-    @GetMapping("/search")
-    public ResponseEntity<ApiResponse<Slice<CardSearchResponseDto>>> searchCard(@AuthenticationPrincipal AuthUser authUser,
-                                                                               @ModelAttribute CardSearchRequestDto searchRequest,
-                                                                               @RequestParam(required = false) Long cursorId,
-                                                                               @RequestParam(defaultValue = "20") int pageSize){
-        Slice<CardSearchResponseDto> result = cardService.searchCard(authUser.getUserId(), searchRequest, cursorId, pageSize);
-        return ResponseEntity.ok(ApiResponse.success(result));
     }
 }

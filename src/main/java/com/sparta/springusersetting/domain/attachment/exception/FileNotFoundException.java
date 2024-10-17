f5642f679dc6be0bc3c6c2b06834b2dd87531580
MinJun Song <77713554+Luta13@users.noreply.github.com>
2024-10-17 15:42:30 +0900

    [fix]FileNotFoundException 오탈자수정

--- src/main/java/com/sparta/springusersetting/domain/attachment/exception/FileNotFoundException.java
+++ src/main/java/com/sparta/springusersetting/domain/attachment/exception/FileNotFoundException.java
@@ -1,6 +1,6 @@
 package com.sparta.springusersetting.domain.attachment.exception;
 import com.sparta.springusersetting.domain.common.exception.GlobalException;
 import static com.sparta.springusersetting.domain.common.exception.GlobalExceptionConst.NOT_FOUND_FILE;
-public class FileFormatException extends GlobalException {
+public class FileNotFoundException extends GlobalException {
     public FileNotFoundException() {super(NOT_FOUND_FILE);}
 }
